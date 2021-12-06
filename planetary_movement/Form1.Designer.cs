0c9f6c1d9b4ddfc39078c1e86fcb88dec3aee7c7
Ridana <dariutja@gmail.com>
2021-12-06 17:27:56 +0300

    Добавьте файлы проекта.

---
+++ planetary_movement/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿
+namespace planetary_movement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mass1 = new System.Windows.Forms.TextBox();
+            this.mass2 = new System.Windows.Forms.TextBox();
+            this.Oy1 = new System.Windows.Forms.TextBox();
+            this.Ox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Oy2 = new System.Windows.Forms.TextBox();
+            this.Ox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Perezapusk = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Speed_y2 = new System.Windows.Forms.TextBox();
+            this.Speed_x2 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.Speed_y1 = new System.Windows.Forms.TextBox();
+            this.Speed_x1 = new System.Windows.Forms.TextBox();
+            this.axis1 = new myUCLib.Axis();
+            this.Axis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(33, 424);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(100, 42);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Масса первой планеты";
+            // 
+            // mass1
+            // 
+            this.mass1.Location = new System.Drawing.Point(33, 22);
+            this.mass1.Name = "mass1";
+            this.mass1.Size = new System.Drawing.Size(100, 20);
+            this.mass1.TabIndex = 3;
+            this.mass1.Text = "34";
+            // 
+            // mass2
+            // 
+            this.mass2.Location = new System.Drawing.Point(33, 65);
+            this.mass2.Name = "mass2";
+            this.mass2.Size = new System.Drawing.Size(100, 20);
+            this.mass2.TabIndex = 4;
+            this.mass2.Text = "650";
+            // 
+            // Oy1
+            // 
+            this.Oy1.Location = new System.Drawing.Point(33, 201);
+            this.Oy1.Name = "Oy1";
+            this.Oy1.Size = new System.Drawing.Size(100, 20);
+            this.Oy1.TabIndex = 6;
+            this.Oy1.Text = "1";
+            // 
+            // Ox1
+            // 
+            this.Ox1.Location = new System.Drawing.Point(33, 158);
+            this.Ox1.Name = "Ox1";
+            this.Ox1.Size = new System.Drawing.Size(100, 20);
+            this.Ox1.TabIndex = 5;
+            this.Ox1.Text = "23";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Масса второй планеты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Координаты первой планеты";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Координаты второй планеты";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(139, 204);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Оу";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(139, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Ох";
+            // 
+            // Oy2
+            // 
+            this.Oy2.Location = new System.Drawing.Point(218, 201);
+            this.Oy2.Name = "Oy2";
+            this.Oy2.Size = new System.Drawing.Size(100, 20);
+            this.Oy2.TabIndex = 15;
+            this.Oy2.Text = "0";
+            // 
+            // Ox2
+            // 
+            this.Ox2.Location = new System.Drawing.Point(218, 158);
+            this.Ox2.Name = "Ox2";
+            this.Ox2.Size = new System.Drawing.Size(100, 20);
+            this.Ox2.TabIndex = 14;
+            this.Ox2.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(324, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Оу";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(324, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Ох";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Perezapusk
+            // 
+            this.Perezapusk.Location = new System.Drawing.Point(218, 424);
+            this.Perezapusk.Name = "Perezapusk";
+            this.Perezapusk.Size = new System.Drawing.Size(100, 42);
+            this.Perezapusk.TabIndex = 18;
+            this.Perezapusk.Text = "перезапуск";
+            this.Perezapusk.UseVisualStyleBackColor = true;
+            this.Perezapusk.Click += new System.EventHandler(this.Perezapusk_Click_1);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(324, 330);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "Vу";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(324, 291);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Vх";
+            // 
+            // Speed_y2
+            // 
+            this.Speed_y2.Location = new System.Drawing.Point(218, 327);
+            this.Speed_y2.Name = "Speed_y2";
+            this.Speed_y2.Size = new System.Drawing.Size(100, 20);
+            this.Speed_y2.TabIndex = 29;
+            this.Speed_y2.Text = "0";
+            // 
+            // Speed_x2
+            // 
+            this.Speed_x2.Location = new System.Drawing.Point(218, 288);
+            this.Speed_x2.Name = "Speed_x2";
+            this.Speed_x2.Size = new System.Drawing.Size(100, 20);
+            this.Speed_x2.TabIndex = 28;
+            this.Speed_x2.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(139, 330);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Vу";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(139, 291);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(19, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Vх";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(215, 259);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(139, 13);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "Скорость второй планеты";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(30, 259);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(140, 13);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "Скорость первой планеты";
+            // 
+            // Speed_y1
+            // 
+            this.Speed_y1.Location = new System.Drawing.Point(33, 327);
+            this.Speed_y1.Name = "Speed_y1";
+            this.Speed_y1.Size = new System.Drawing.Size(100, 20);
+            this.Speed_y1.TabIndex = 23;
+            this.Speed_y1.Text = "0";
+            // 
+            // Speed_x1
+            // 
+            this.Speed_x1.Location = new System.Drawing.Point(33, 288);
+            this.Speed_x1.Name = "Speed_x1";
+            this.Speed_x1.Size = new System.Drawing.Size(100, 20);
+            this.Speed_x1.TabIndex = 22;
+            this.Speed_x1.Text = "1";
+            // 
+            // axis1
+            // 
+            this.axis1.AllowDrop = true;
+            this.axis1.axis_bkcolor = System.Drawing.Color.White;
+            this.axis1.axis_color = System.Drawing.Color.Gray;
+            this.axis1.Axis_Type = ((byte)(1));
+            this.axis1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.axis1.E_x = 0;
+            this.axis1.E_y = 0;
+            this.axis1.Location = new System.Drawing.Point(398, 22);
+            this.axis1.Name = "axis1";
+            this.axis1.Pix_color = System.Drawing.Color.Black;
+            this.axis1.Pix_Size = 2F;
+            this.axis1.Pix_type = ((byte)(1));
+            this.axis1.Size = new System.Drawing.Size(912, 527);
+            this.axis1.TabIndex = 32;
+            this.axis1.x_Base = 1F;
+            this.axis1.x_Name = "X";
+            this.axis1.y_Base = 1F;
+            this.axis1.y_Name = "Y";
+            this.axis1.z_Base = 1F;
+            this.axis1.z_Name = "Z";
+            // 
+            // Axis
+            // 
+            this.Axis.Location = new System.Drawing.Point(142, 496);
+            this.Axis.Name = "Axis";
+            this.Axis.Size = new System.Drawing.Size(75, 23);
+            this.Axis.TabIndex = 33;
+            this.Axis.Text = "Оси";
+            this.Axis.UseVisualStyleBackColor = true;
+            this.Axis.Click += new System.EventHandler(this.Axis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 562);
+            this.Controls.Add(this.Axis);
+            this.Controls.Add(this.axis1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.Speed_y2);
+            this.Controls.Add(this.Speed_x2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.Speed_y1);
+            this.Controls.Add(this.Speed_x1);
+            this.Controls.Add(this.Perezapusk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Oy2);
+            this.Controls.Add(this.Ox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Oy1);
+            this.Controls.Add(this.Ox1);
+            this.Controls.Add(this.mass2);
+            this.Controls.Add(this.mass1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Движение планет";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mass1;
+        private System.Windows.Forms.TextBox mass2;
+        private System.Windows.Forms.TextBox Oy1;
+        private System.Windows.Forms.TextBox Ox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Oy2;
+        private System.Windows.Forms.TextBox Ox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Perezapusk;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox Speed_y2;
+        private System.Windows.Forms.TextBox Speed_x2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox Speed_y1;
+        private System.Windows.Forms.TextBox Speed_x1;
+        private myUCLib.Axis axis1;
+        private System.Windows.Forms.Button Axis;
+    }
+}
+
---
+++ planetary_movement/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using myUCLib;
+
+namespace planetary_movement
+{
+    public partial class Form1 : Form
+    {
+        const float G = 6.67e-11F;
+        float x01, y01, x02, y02;
+        float v0x1, v0x2, v0y1, v0y2;
+        float R;
+        float ax1, ay1, ax2, ay2;
+        float dt = 0.01F;
+        float m1, m2;
+
+        private void Axis_Click(object sender, EventArgs e)
+        {
+            x01 = Convert.ToSingle(Ox1.Text);
+            y01 = Convert.ToSingle(Oy1.Text);
+            x02 = Convert.ToSingle(Ox2.Text);
+            y02 = Convert.ToSingle(Oy2.Text);
+            axis1.Axis_Type = 1;
+            axis1.x_Name = "Ox";
+            axis1.y_Name = "Oy";
+            axis1.x_Base = (float)Math.Abs(x01 * 10);
+            axis1.y_Base = (float)Math.Abs(y01 * 10);
+            axis1.AxisDraw();
+            axis1.DinToPic();
+            axis1.StatToDin();
+            Invalidate();
+            axis1.Pix_Size = 0.01f;
+            axis1.PixDraw(x01,y01, Color.Green, 1);
+            axis1.StatToDin();
+            axis1.Pix_Size = 0.02f;
+            axis1.DinToPic();
+            axis1.PixDraw(x02,y02, Color.Blue, 1);
+            axis1.StatToDin();
+            axis1.DinToPic();
+        }
+
+        float Fx2, Fx1, Fy2, Fy1;
+        float x1, x2, y1, y2;
+        float vx1, vx2, vy1, vy2;
+        float xc, yc, vxc, vyc;
+        float x1next, y1next, x2next, y2next;
+        float vx1next, vy1next, vx2next, vy2next;
+
+        private void Perezapusk_Click_1(object sender, EventArgs e)
+        {
+            int z = 0;
+            mass1.Text = Convert.ToString(z);
+            mass2.Text = Convert.ToString(z);
+            Ox1.Text = Convert.ToString(z);
+            Ox2.Text = Convert.ToString(z);
+            Oy1.Text = Convert.ToString(z);
+            Oy2.Text = Convert.ToString(z);
+            Speed_x1.Text = Convert.ToString(z);
+            Speed_x2.Text = Convert.ToString(z);
+            Speed_y1.Text = Convert.ToString(z);
+            Speed_y2.Text = Convert.ToString(z);
+            axis1.ClearDin();
+            axis1.ClearPic();
+            timer1.Enabled = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                m1 = Convert.ToSingle(mass1.Text);
+                m2 = Convert.ToSingle(mass2.Text);
+                v0x1 = Convert.ToSingle(Speed_x1.Text);
+                v0y1 = Convert.ToSingle(Speed_y1.Text);
+                v0x2 = Convert.ToSingle(Speed_x2.Text);
+                v0y2 = Convert.ToSingle(Speed_y2.Text);
+
+                if (m1 < 0 || m2 < 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Уважаемый пользователь, пожалуйста, введите все величины в виде положительного числа");
+                }
+            }
+            catch (FormatException)
+            {
+                timer1.Stop();
+                MessageBox.Show("Уважаемый пользователь, пожалуйста, введите ЧИСЛО!");
+                int z = 0;
+                m1 = Convert.ToSingle(z);
+                m2 = Convert.ToSingle(z);
+                x01 = Convert.ToSingle(z);
+                y01 = Convert.ToSingle(z);
+                x02 = Convert.ToSingle(z);
+                y02 = Convert.ToSingle(z);
+                v0x1 = Convert.ToSingle(z);
+                v0y1 = Convert.ToSingle(z);
+                v0x2 = Convert.ToSingle(z);
+                v0y2 = Convert.ToSingle(z);
+                return;
+            }
+            x1next = x01;
+            x2next = x02;
+            y1next = y01;
+            y2next = y02;
+            vx1next = v0x1;
+            vy1next = v0y1;
+            vx2next = v0x2;
+            vy2next = v0y2;
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            R = (float)Math.Sqrt((float)(Math.Pow((x2next - x1next), 2) + Math.Pow((y2next - y1next), 2)));
+            vxc = (vx1 * m1 + vx2 * m2) / (m1 + m2);
+            vyc = (vy1 * m1 + vy2 * m2) / (m1 + m2);
+
+            Fx1 = (-G * m1 * m2 * (x2 - x1)) / ((float)Math.Pow(R, 3));
+            Fy1 = (-G * m1 * m2 * (y2 - y1)) / ((float)Math.Pow(R, 3));
+            Fx2 = -Fx1;
+            Fy2 = -Fy1;
+            ax1 = Fx1 / m1;
+            ax2 = Fx2 / m2;
+            ay1 = Fy1 / m1;
+            ay2 = Fy2 / m2;
+            vx1 = vx1next - vxc + ax1 * dt;
+            vy1 = vy1next - vyc + ay1 * dt;
+            vx2 = vx2next - vxc + ax2 * dt;
+            vy2 = vy2next - vyc + ay2 * dt;
+            x1 = (float)(x1next + vx1 * dt + ax1 * 0.5 * Math.Pow(dt, 2));
+            y1 = (float)(y1next + vy1 * dt + ay1 * 0.5 * Math.Pow(dt, 2));
+            x2 = (float)(x2next + vx2 * dt + ax2 * 0.5 * Math.Pow(dt, 2));
+            y2 = (float)(y2next + vy2 * dt + ay2 * 0.5 * Math.Pow(dt, 2));
+            axis1.Pix_Size = 0.01f;
+            axis1.PixDraw((float)(x1 - xc), (float)(y1 - yc), Color.Green, 1);
+            axis1.StatToDin();
+            axis1.Pix_Size = 0.015f;
+            axis1.PixDraw((float)(x1 - xc), (float)(y1 - yc), Color.Black, 2);
+            axis1.DinToPic();
+            axis1.Pix_Size = 0.015f;
+            axis1.PixDraw((float)(x2 - xc), (float)(y2 - yc), Color.Gray, 1);
+            axis1.Pix_Size = 0.02f;
+            axis1.StatToDin();
+            axis1.PixDraw((float)(x2 - xc), (float)(y2 - yc), Color.Blue, 2);
+            axis1.DinToPic();
+            x1next = x1;
+            x2next = x2;
+            y1next = y1;
+            y2next = y2;
+            vx1next = vx1;
+            vx2next = vx2;
+            vy1next = vy1;
+            vy2next = vy2;
+        }
+    }
+} 
+
+
+//скорость центра масс и центр масс(х1*м1+х2м2)/2
+//отображение в системе центра масс. координаты пример х1-хс1
